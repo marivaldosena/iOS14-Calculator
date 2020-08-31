01d4b320152b91d5ef2c9ef3e3c4f36df140a3c0
Marivaldo Sena <marivaldo.sena@live.com>
2020-08-31 15:20:18 -0300

    Initial commit with calculator layout.

---
+++ Test/ViewController.swift
@@ -0,0 +1,104 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Marivaldo Sena on 31/08/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    //MARK: - IBOutlets
+    @IBOutlet weak var zeroButton: UIButton!
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    @IBOutlet weak var nineButton: UIButton!
+    
+    @IBOutlet weak var commaButton: UIButton!
+    
+    @IBOutlet weak var additionButton: UIButton!
+    @IBOutlet weak var subtractionButton: UIButton!
+    @IBOutlet weak var multiplicationButton: UIButton!
+    @IBOutlet weak var divisionButton: UIButton!
+    @IBOutlet weak var percentButton: UIButton!
+    @IBOutlet weak var resultButton: UIButton!
+    
+    @IBOutlet weak var acButton: UIButton!
+    @IBOutlet weak var negationButton: UIButton!
+    
+    
+    //MARK: - Life Cycle Events - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        zeroButton.layer.cornerRadius = 20
+    }
+    
+    //MARK: - Numbers' buttons
+    
+    @IBAction func zeroButtonPressed(_ sender: UIButton) {
+    }
+ 
+    @IBAction func oneButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func threeButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func fourButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func fiveButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func sixButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func sevenButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func eightButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func nineButtonPressed(_ sender: UIButton) {
+    }
+    
+    //MARK: - Operations' buttons
+    
+    @IBAction func additionButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func subtractionButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func multiplicationButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func divisionButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func percentButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func resultButtonPressed(_ sender: UIButton) {
+    }
+    
+    //MARK: - Special operations' buttons
+    
+    @IBAction func commaButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func acButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func negationButtonPressed(_ sender: UIButton) {
+    }
+    
+}
+
